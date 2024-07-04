9525b351e3eed1c4b7485c68d4cea9077580369a
Sobilo34 <bilalsolih60@gmail.com>
2024-07-04 11:25:21 +0100

    returns the sum of two floats

--- 0-add.py
+++ 0-add.py
@@ -7,6 +7,6 @@ float b as arguments and returns their sum as a float
 
 def add(a: float, b: float) -> float:
     """
-    Returns the sum of two floats
+    It returns the sum of two floats
     """
     return a + b
