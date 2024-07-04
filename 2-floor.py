b61d1588c24258977da43ceaa666a89b233839ca
Sobilo34 <bilalsolih60@gmail.com>
2024-07-04 11:32:23 +0100

    returns the floor of a float

---
+++ 2-floor.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+A type-annotated function floor which takes afloat n as argument
+and returns the floor of the float.
+"""
+
+
+def floor(n: float) -> int:
+    """
+    It returns the floor of a float
+    """
+    return int(n)
