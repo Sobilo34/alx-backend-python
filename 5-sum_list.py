ce0305d8eb0093153c58c1e8ee5b3770812d7958
Sobilo34 <bilalsolih60@gmail.com>
2024-07-04 11:47:18 +0100

    It returns the sum of a list of floats

---
+++ 5-sum_list.py
@@ -0,0 +1,12 @@
+#!/usr/vin/python3
+"""
+A type-annotated function sum_list which takes a list input_list of
+floats as argument and returns their sum as a float.
+"""
+
+
+def sum_list(input_list: list[float]) -> float:
+    """
+    It returns the sum of a list of floats
+    """
+    return sum(input_list)
