2f14886b4436cbceb79359b3487eb275e250891c
Sobilo34 <bilalsolih60@gmail.com>
2024-07-04 12:12:03 +0100

    a tuple containing the string k and the square of the number v.

---
+++ 7-to_kv.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+A type-annotated function to_kv that takes a string k
+and an int OR float v as arguments and returns a tuple. 
+"""
+
+from typing import Union, Tuple
+
+def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
+    """
+    Returns a tuple containing the string k and the square of the number v.
+    """
+    return (k, v * v)
