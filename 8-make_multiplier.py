2450726e3e640fdd3cb3f61e0b8ec4096c749ebe
Sobilo34 <bilalsolih60@gmail.com>
2024-07-04 12:16:54 +0100

    Returns a function that multiplies a float by multiplier.

---
+++ 8-make_multiplier.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+A type-annotated function make_multiplier that takes a
+float multiplier as argument and returns a function that
+multiplies a float by multiplier.
+"""
+from typing import Callable
+
+
+def make_multiplier(multiplier: float) -> Callable[[float], float]:
+    """
+    Returns a function that multiplies a float by multiplier.
+    """
+    def multiplier_fn(n: float) -> float:
+        return n * multiplier
+    return multiplier_fn
